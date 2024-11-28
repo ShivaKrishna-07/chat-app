7a00ce838577382d83a26a4a30dc4c95699005e4
Shiva Krishna <shivakrishna6032@gmail.com>
2024-11-28 22:45:53 +0530

    complete auth setup

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import { login, logout, signup } from '../controllers/auth.controller.js';
+
+import { protectRoute } from '../middlewares/auth.middleware.js';
+import { checkAuth, login, logout, signup, updateRoute } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/login",  login);
 router.post("/logout", logout);
+
+router.post("/update-profile", protectRoute, updateRoute);
+router.get("/check", protectRoute, checkAuth)
 
 export default router;
