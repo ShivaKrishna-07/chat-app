d33e89bf876ff68c14bbecbdf5dd0e8ec70b14e8
Shiva Krishna <shivakrishna6032@gmail.com>
2024-11-28 21:28:03 +0530

    create auth routes and controllers at server

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import cors from 'cors'
+import authRoutes from './routes/auth.route.js'
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors({ origin: true }));
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`);
+    connectDB();
+})
