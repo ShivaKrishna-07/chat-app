6ea61c276fe31b923df25fefa0f5ace1d0e5de43
Shiva Krishna <shivakrishna6032@gmail.com>
2024-12-01 17:45:08 +0530

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,37 @@
-import express from 'express'
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import { connectDB } from './lib/db.js';
-import authRoutes from './routes/auth.route.js'
-import messageRoute from './routes/message.route.js'
-import { app, server } from './lib/socket.js';
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoute)
+app.get("/", (req, res) => {
+  res.send("Hello, this is your Express backend!");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoute);
 
 server.listen(PORT, () => {
-    console.log(`Server is running on Port ${PORT}`);
-    connectDB();
-})
+  console.log(`Server is running on Port ${PORT}`);
+  connectDB();
+});
+
+export default app;
